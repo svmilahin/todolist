8b5bd03f91f4179f2041b167fe8cd56305b13171
sv_milahin <sv_milahin@vsh1.ru>
2023-08-17 22:49:07 +0700

    fix urls

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bot/', include('bot.urls')),
-    path('core/', include('core.urls', namespace='core')),
-    path('goals/', include('goals.urls', namespace='goals')),
+    path('core/', include('core.urls')),
+    path('goals/', include('goals.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger-ui/',
